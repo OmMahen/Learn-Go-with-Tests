ec40b566c0b7360561b21368c12ab6886b72da07
OmMahen <mahendraputraraharja@gmail.com>
2024-08-30 14:05:32 +0700

    Empty String Handler

--- hello_test.go
+++ hello_test.go
@@ -3,8 +3,20 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Mahen")
-	want := "Hello, Mahen"
+	t.Run("Saying hello to people", func(t *testing.T) {
+		got := Hello("Mahen")
+		want := "Hello, Mahen"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("Say 'Hello, World' when empty string is passed", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper() // Mark this method as a helper so failures report the function call line, not the helper.
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
 	}
